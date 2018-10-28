92650064abd15879860f2efda6a193c232ef3512
Nathan Pillai <natepill@gmail.com>
2018-10-28 12:57:36 -0700

    attempting tuples

--- challenge-set-2/histogram-list-of-tuples.py
+++ challenge-set-2/histogram-list-of-tuples.py
@@ -7,18 +7,11 @@ with open('blog-text') as file:
 
 
 def histogram(words):
-    wordlists = []
-    for word in words:
-        if word not in wordlists:
-            wordlists.append([word, 1])
-        for item in wordlists:
-            if item[0] == word:
-                item[1] += 1
-    return wordlists
+
 
 # filename = sys.argv[1]
 # words = open(sys.argv[1], "r").read().split()
-print(histogram(blog))
+print(histogram(words))
 
 # histogram(blog)
 # print(unique_words(histogram(blog)))
