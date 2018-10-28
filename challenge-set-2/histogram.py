7a50202dc4904738682b450cf949b7eefafd8511
Nathan Pillai <natepill@gmail.com>
2018-10-27 20:08:04 -0700

    template done

---
+++ challenge-set-2/histogram.py
@@ -0,0 +1,16 @@
+
+with open('blog-text') as file:
+    blog = file.read().split()
+    print(blog)
+
+def histogram():
+     # stores each unique word along with the number of times the word appears in the source text
+    pass
+
+def unique_words():
+    # takes a histogram argument and returns the total count of unique words in the histogram.
+    pass
+
+def frequency():
+     # takes a word and histogram argument and returns the number of times that word appears in a text. 
+    pass
